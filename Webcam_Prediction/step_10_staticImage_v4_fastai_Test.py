86937083594102aad252fbaed45ed9e737825911
Jannu Sudhakar <babinatusk@gmail.com>
2022-06-03 17:21:50 +0000

    commiting for local testing

--- Webcam_Prediction/step_10_staticImage_v4_fastai_Test.py
+++ Webcam_Prediction/step_10_staticImage_v4_fastai_Test.py
@@ -22,11 +22,11 @@ image = cv2.imread(filename, 0)
 
 # start_point = (700, 275)
 # end_point = (950, 525)
-start_point = (y1, x1)
-end_point = (y2, x2)
-color = (0, 0, 0)
-thickness = 2
-image = cv2.rectangle(image, start_point, end_point, color, thickness)
+#start_point = (y1, x1)
+#end_point = (y2, x2)
+#color = (0, 0, 0)
+#thickness = 2
+#image = cv2.rectangle(image, start_point, end_point, color, thickness)
 
 # cv2.imwrite("12_withRectangle.png", image)
 
@@ -34,10 +34,11 @@ image = cv2.rectangle(image, start_point, end_point, color, thickness)
 
 # crop_img = image[275:525, 700:950]
 crop_img = image[x1:x2, y1:y2]
+crop_img = cv2.resize(crop_img, (50, 50))
 
-cv2.imwrite("intermediate.png", crop_img)
+cv2.imwrite("public/intermediate.png", crop_img)
 
-img = open_image('intermediate.png')
+img = open_image('public/intermediate.png')
 
 
 pred_class,pred_idx,outputs = learn_inf.predict(img)
@@ -47,4 +48,4 @@ list_abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
 
 print (list_abc[int(list[pred_idx])])
 
-os.remove('intermediate.png')
+#os.remove('intermediate.png')

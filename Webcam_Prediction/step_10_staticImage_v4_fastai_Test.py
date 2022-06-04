925689fe2c543b0b501bffd6501d227fd158aabf
good-soul <jay.vora@cern.ch>
2022-06-04 03:00:41 -0400

    cosmetic changes

--- Webcam_Prediction/step_10_staticImage_v4_fastai_Test.py
+++ Webcam_Prediction/step_10_staticImage_v4_fastai_Test.py
@@ -3,6 +3,7 @@
 # Run as "python step_10_staticImage_v4_fastai_Test.py 1_rawinput.png 275 525 700 950"
 #
 
+import time
 import os
 import sys
 import cv2
@@ -15,31 +16,41 @@ x2 = int(sys.argv[3])
 y1 = int(sys.argv[4])
 y2 = int(sys.argv[5])
 
-learn_inf = load_learner('export_50_no_tfms/')
-# img = open_image('4_grayscale.png')
+# learn_inf = load_learner('export_50_no_tfms/')
+learn_inf = load_learner('export_100_tfms/')
+# image = open_image('intermediate.png')
+# image = cv2.imread('intermediate.png', 0)
 image = cv2.imread(filename, 0)
 
 
 # start_point = (700, 275)
 # end_point = (950, 525)
-#start_point = (y1, x1)
-#end_point = (y2, x2)
-#color = (0, 0, 0)
-#thickness = 2
-#image = cv2.rectangle(image, start_point, end_point, color, thickness)
+# start_point = (y1, x1)
+# end_point = (y2, x2)
+start_point = (0, 0)
+end_point = (205, 205)
+color = (0, 0, 0)
+thickness = 2
+# image = cv2.rectangle(image, start_point, end_point, color, thickness)
 
 # cv2.imwrite("12_withRectangle.png", image)
 
 # clone = image.copy()
 
 # crop_img = image[275:525, 700:950]
-crop_img = image[x1:x2, y1:y2]
-crop_img = cv2.resize(crop_img, (50, 50))
+# crop_img = image[x1:x2, y1:y2]
+# crop_img = image.copy()
 
-cv2.imwrite("public/intermediate.png", crop_img)
+crop_img = cv2.resize(image, (50, 50))
+# crop_img = crop_img.reshape(1, 1, 50, 50).astype(np.float32)
 
-img = open_image('public/intermediate.png')
+tmpFile = str(int(time.time()))+'.png'
 
+# cv2.imwrite(tmpFile, crop_img)
+cv2.imwrite("intermediate2.png", crop_img)
+
+# img = open_image(tmpFile)
+img = open_image("intermediate2.png")
 
 pred_class,pred_idx,outputs = learn_inf.predict(img)
 
@@ -48,4 +59,4 @@ list_abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
 
 print (list_abc[int(list[pred_idx])])
 
-#os.remove('intermediate.png')
+# os.remove(tmpFile)
